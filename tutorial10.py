23b38173a28fe02bce343b422e69783fc509e9a4
mausamkumar-27 <aapka_github_email@gmail.com>
2026-08-28 06:22:35 +0000

    28 Aug 2026, 11:52

---
+++ tutorial10.py
@@ -0,0 +1,7 @@
+name=input("Enter Your Name: ")
+code=input("Your code is: ")
+print(name + code)
+a=input("Enter 1st No: ")
+b=input("Enter 2nd No: ")
+print(a+b)    #abhi a & b as string hai so O/P 12100   
+print(int(a)+int(b))   #ab ise typecast kr diya hai
