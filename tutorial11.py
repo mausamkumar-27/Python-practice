646b9f8f83be72739fdf6bb076b069323fae0b58
mausamkumar-27 <aapka_email@gmail.com>
2026-08-28 12:53:29 +0530

    use of string as index and as loop

--- tutorial11.py
+++ tutorial11.py
@@ -3,11 +3,11 @@ print(teacher)
 myself='''I am Mausam.I joind college
 as a fresher.I have keen interest in Computer Science.
 Especially,in the field of AI. '''
-print(myself)         #ye saare index wale eg hai
+print(myself)         #ye saare index wale eg hai samjha
 print(myself[0])
 print(myself[1])
 print(myself[2])
 print(myself[3])
 print(myself[4])
 for character in myself :
-     print(character)
+     print(character)      #saare character ko print krega index lagane ki jarurat nhin hai
