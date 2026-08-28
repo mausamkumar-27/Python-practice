f432b1435f2b29f81f9e8436d9e677ca19ba756a
mausamkumar-27 <aapka_email@gmail.com>
2026-08-28 07:20:57 +0000

    28 Aug 2026, 12:50

--- tutorial11.py
+++ tutorial11.py
@@ -3,4 +3,11 @@ print(teacher)
 myself='''I am Mausam.I joind college
 as a fresher.I have keen interest in Computer Science.
 Especially,in the field of AI. '''
-print(myself)
+print(myself)         #ye saare index wale eg hai
+print(myself[0])
+print(myself[1])
+print(myself[2])
+print(myself[3])
+print(myself[4])
+for character in myself :
+     print(character)
