6b5dea5acdc31a1a4d7f7be7fc1043ebd6e31755
mausamkumar-27 <aapka_email@gmail.com>
2026-08-28 07:15:18 +0000

    28 Aug 2026, 12:45

--- tutorial11.py
+++ tutorial11.py
@@ -1,3 +1,6 @@
+teacher="Introduce your self: "
+print(teacher)
 myself='''I am Mausam.I joind college
 as a fresher.I have keen interest in Computer Science.
 Especially,in the field of AI. '''
+print(myself)
