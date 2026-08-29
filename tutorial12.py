94e266fdde88ea7131afea5cf858d7ff545161b6
mausamkumar-27 <aapka_email@gmail.com>
2026-08-29 19:37:53 +0000

    30 Aug 2026, 01:07

--- tutorial12.py
+++ tutorial12.py
@@ -1,8 +1,8 @@
-a="Mausam"
+a="Mango"
 print(len(a))
 print(a[:4])
 print(a[:])
-print(a[1:5])
-print(a[:-4])
-print(a[-4:-5])
-print(a[-5:-4])
+print(a[1:])
+print(a[:-3])
+print(a[-1:-3])
+print(a[-3:-1])
