b4a9d1376277c4177ec433dcd53590b311968712
mausamkumar-27 <aapka_email@gmail.com>
2026-08-29 20:01:41 +0000

    30 Aug 2026, 01:31

--- tutorial13.py
+++ tutorial13.py
@@ -1,2 +1,8 @@
-a="i am mausam"
-print(a.uppercase())
+a="i am mAuSaM"
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+print(a.rstrip("aM"))
+print(a.replace(""))
