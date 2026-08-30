e84fd0245b49564014a6b015e64b023374c7600e
mausamkumar-27 <aapka_email@gmail.com>
2026-08-30 03:34:20 +0000

    30 Aug 2026, 09:04

---
+++ tutorial15.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a No. "))
+if num > 0:
+    if num%2==0:
+        print("+ve Even",num)
+    else:
+        print("+ve Odd",num) 
+elif num==0:
+    print("Zero",num)
+else:
+    print("-ve",num)    
+
+          
