ad3b1fe1346cfb89642812330b1c8f24affd00e0
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 16:32:00 +0530

    for loop

--- tutorial17.py
+++ tutorial17.py
@@ -1,11 +1,8 @@
-'''name="Mausam"
+name="Mausam"
 for i in name:
     print(i)
-'''
+
 colors=["Red","Green","Blue"]
-items=["Apple","Orange","Banana"]
 for color in colors :
- for item in items:
   print(color)
-  print(item)
 
