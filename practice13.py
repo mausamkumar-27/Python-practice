4247299679558a369d30355dcbfe69d545ef840b
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 11:20:45 +0000

    31 Aug 2026, 16:50

--- practice13.py
+++ practice13.py
@@ -2,5 +2,5 @@ self="Mausam is laborious boy"
 print(self.find("is"))
 print(self.find("bbb"))
 print(self.index("laborious"))
-print(self.index("baby"))
+#print(self.index("baby"))
 print(self.count("a"))
