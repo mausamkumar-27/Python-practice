2db1231fceef131c686b7403e58529ff45c4fb25
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 12:25:09 +0000

    31 Aug 2026, 17:55

--- practicetill17a.py
+++ practicetill17a.py
@@ -9,6 +9,19 @@ elif n==0:
     print("Zero",n)
 else:
     print("Negative")    
-'''
+
 for i in range(10,2,-2):
-      print(i)        
+    print(i)
+'''
+x=int(input("Enter the No: "))
+match x:
+    case 2:
+        print("x is two")
+    case 3:
+        print("x is Three")  
+    case _ if x<0:
+        print("x is Negative")   
+    case _ if x%2==0:
+        print("x is Even") 
+    case _ :
+        print("Not Matching")              
