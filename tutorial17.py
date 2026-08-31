1961e0a2cb71dfc60a439d0b9fc0c644471d2062
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 11:00:26 +0000

    31 Aug 2026, 16:30

--- tutorial17.py
+++ tutorial17.py
@@ -1,7 +1,11 @@
-name="Mausam"
+'''name="Mausam"
 for i in name:
     print(i)
-    colors=["Red","Green","Blue"]
-    for color in colors :
-        print(color)
+'''
+colors=["Red","Green","Blue"]
+items=["Apple","Orange","Banana"]
+for color in colors :
+ for item in items:
+  print(color)
+  print(item)
 
