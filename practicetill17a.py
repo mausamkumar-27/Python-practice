3b7cb7c3e97aabc026ab84f03927254d6b996399
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 12:03:18 +0000

    31 Aug 2026, 17:33

--- practicetill17a.py
+++ practicetill17a.py
@@ -1,2 +1,9 @@
 n=input("Enter the Number: ")
 print(int(n))
+if n>0 :
+    print("Positive",n)
+    if n%2==0:
+        print("Positive even",n)
+    else:
+        print("Positive odd",n)  
+          
