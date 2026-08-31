6482efa4f07ba849f568cb9d38d7d0bd5c5bbcd8
mausamkumar-27 <aapka_email@gmail.com>
2026-08-31 11:42:03 +0000

    31 Aug 2026, 17:12

--- practicetill17.py
+++ practicetill17.py
@@ -5,5 +5,5 @@ print(text[-4:-1])
 s="Harry"
 print(s[::-1])       #Reverse wala pta kr
 a="###Hello World###"
-print(a.strip())       #strip wala pta kr
+print(a.strip("#") )      #strip wala pta kr
 print(a.rstrip("#"))
