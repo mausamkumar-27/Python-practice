e336aba72308d055d453a81ed6ad1c5cf0c50b50
mausamkumar-27 <aapka_email@gmail.com>
2026-09-02 15:52:28 +0000

    2 Sept 2026, 21:22

--- tutorial22.py
+++ tutorial22.py
@@ -3,3 +3,13 @@ print(l)
 print(type(l))
 print(l[2])
 print(l[-3])
+if "Mausam" in l:
+    print("Mausam is present ")
+else:
+    print("Mausam is absent ")
+print(l[3]) 
+if 5 in 52:
+    print("yes")
+else:
+    print("no")    
+
