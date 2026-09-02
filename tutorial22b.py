c0a5db8ffc7e81fa5664babd03c58263e44efd76
mausamkumar-27 <aapka_email@gmail.com>
2026-09-02 17:08:32 +0000

    2 Sept 2026, 22:38

--- tutorial22b.py
+++ tutorial22b.py
@@ -1,5 +1,8 @@
 colors=["Red","Green","Blue"]
+clothes=["Shirt","Pants","Vest"]
 colors.append("Yellow")
 print(colors)
 colors.insert(1,"Orange")
+print(colors)
+colors.extend(clothes)
 print(colors)
