feb01c94b66394153bacffde4dd033845acd5fbb
mausamkumar-27 <aapka_email@gmail.com>
2026-09-02 18:38:19 +0000

    3 Sept 2026, 00:08

--- tutorial20.py
+++ tutorial20.py
@@ -5,5 +5,8 @@ name(last_name="Kumar",first_name="Mausam")
 
 
 def average(a=9,b=1):
-    print("The averaage is: "(a+b)/2)
+    print("The averaage is: ",(a+b)/2)
+average()
+average(5)
 average(b=9)   
+average(1,5)
