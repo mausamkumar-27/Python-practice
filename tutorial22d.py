a939491c4f450ecbe5510978bd52f87d6efceb78
mausamkumar-27 <aapka_email@gmail.com>
2026-09-02 17:38:26 +0000

    2 Sept 2026, 23:08

--- tutorial22d.py
+++ tutorial22d.py
@@ -1,2 +1,11 @@
-words=["Mausam","Apple","Boy","Girl"]
+words=["Mausam","Apple","Boy","Girl","Boy","Boy","Boy"]
 print(words.index("Boy"))
+print(words.count("Boy"))
+
+
+
+a=[45,20,29]
+b=a
+b[0]=90
+print(a)
+print(b)
