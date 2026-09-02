e4039207cda2e31377d858cfcfb8872bfd63d3f4
mausamkumar-27 <aapka_email@gmail.com>
2026-09-02 18:14:20 +0000

    2 Sept 2026, 23:44

--- tutorial20.py
+++ tutorial20.py
@@ -1,5 +1,5 @@
-a=int(input("Enter 1st No. "))
-b=int(input("Enter 2nd No. "))
-def average(a,b):
+#a=int(input("Enter 1st No. "))
+#b=int(input("Enter 2nd No. "))
+def average(a=,b=2):
  print("The average is: ",(a+b)/2)
- average(a,b)
+ #average()
