e07eed2dbe095ae675405d6f5f4170a54277f3cc
mausamkumar-27 <aapka_email@gmail.com>
2026-09-03 13:08:46 +0000

    3 Sept 2026, 18:38

--- tutorial20.py
+++ tutorial20.py
@@ -6,8 +6,9 @@ name(last_name="Kumar",first_name="Mausam")
 a=int(input("Enter a No. "))
 b=int(input("Enter  No. "))
 
-def average(a,b):
+def average(a=9,b=7):
     print("The averaage is: ",(a+b)/2)
 average(a,b)
 average(a,b=5)
+average(a=5,b=9)
 
