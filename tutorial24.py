aaee07535b595446badf496cc6bbf2ca63184e55
mausamkumar-27 <aapka_email@gmail.com>
2026-09-04 06:48:45 +0000

    4 Sept 2026, 12:18

--- tutorial24.py
+++ tutorial24.py
@@ -2,3 +2,9 @@ a=(1,"Mausam",True,9,11,34)
 print(type(a))
 b=(1,)
 print(type(b))
+
+name=("Mausam","Harry","Mansi","Aman","India")
+if "Mausam" in name:
+    print("Yes,Mausam is Present")
+else:
+    print("No,Mausam is absent")    
