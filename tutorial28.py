dd0d824c7b55a75f3f2afa6b2a3dff6ca073a737
mausamkumar-27 <aapka_email@gmail.com>
2026-09-04 16:38:47 +0530

    F-string case

--- tutorial28.py
+++ tutorial28.py
@@ -2,3 +2,8 @@ name="Mausam Kumar"
 country="India"
 sen=f"My name is {name} and I am from {country}:"
 print(sen)
+
+
+father="Dilip Kumar Yadav"
+mother="Sabita Devi"
+print(F"My father name is {father}.\nMy mother name is {mother}.\n")
