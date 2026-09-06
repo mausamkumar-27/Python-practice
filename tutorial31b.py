dbb4ab5cacfa278ceb77c32a023e65fd10913796
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:20:13 +0000

    7 Sept 2026, 01:50

--- tutorial31b.py
+++ tutorial31b.py
@@ -6,4 +6,7 @@ city1.intersection_update(city2)
 print(city1)'''
 
 
-print(city1.symmetric_difference(city2))
+#print(city1.symmetric_difference(city2))
+#print(city1.symmetric_difference_update(city2))     sir why again return o/p as None
+city1.symmetric_difference_update(city2)
+print(city1)
