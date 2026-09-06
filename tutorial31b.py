1215506ca41da7dbfe82e29437bf26ab2519de6e
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:22:51 +0000

    7 Sept 2026, 01:52

--- tutorial31b.py
+++ tutorial31b.py
@@ -10,3 +10,5 @@ print(city1)'''
 #print(city1.symmetric_difference_update(city2))     sir why again return o/p as None
 '''city1.symmetric_difference_update(city2)
 print(city1)'''
+city1.difference_update(city2)
+print(city1)
