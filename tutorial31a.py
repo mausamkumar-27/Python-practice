9c81df85e0057bee6ef61c9371c67be6b7342687
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:07:40 +0000

    7 Sept 2026, 01:37

--- tutorial31a.py
+++ tutorial31a.py
@@ -1,11 +1,11 @@
-'''s1={"Mausam",2,4,2,4,5,6,65,20}
+s1={"Mausam",2,4,2,4,5,6,65,20}
 s2={"Kumar",2,4,3,4,6,5,65,19,76}
 print(s1.union(s2))
 #print(s1.update(s2)).       Sir why return o/p as None
 s1.update(s2)
 print(s1)
 
-#print(s1.intersection(s2)).      ye 'Kumar',19'76 etc. dega bcz s2 ka sara item uodate kr s1 me daal chuka hai'''
+#print(s1.intersection(s2)).      ye 'Kumar',19'76 etc. dega bcz s2 ka sara item uodate kr s1 me daal chuka hai
 
 city1={"Delhi","Mumbai","Londan","Hydrabad","Kota"}
 city2={"Patna","Indore","Wizag","Kota","Londan"}
