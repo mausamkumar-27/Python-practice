c3768db2d4f74282bb7d86016586c076008998c6
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:46:50 +0000

    7 Sept 2026, 02:16

--- tutorial31d.py
+++ tutorial31d.py
@@ -1,4 +1,15 @@
 city1={"Tokyo","Madrid","Berlin","Delhi"}
 city2={"Tokyo","Seoul","Kabul","Madrid"}
-city1.pop("Tokyo")
+'''city1.pop("Tokyo")         TypeError bcz tum predict nhi kr skte ki kon sa item poped hoga(unordered,unindexed)
+print(city1)'''
+
+city1.pop()
+print(city1)
+
+#print(city2.clear())             why return o/p  as None
+city2.clear()
+print(city2)
+
+
+del city1
 print(city1)
