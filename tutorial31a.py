cc668a23afc5e5f4ec54def87b7b098230780d2d
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 19:21:12 +0000

    7 Sept 2026, 00:51

--- tutorial31a.py
+++ tutorial31a.py
@@ -1,6 +1,6 @@
 s1={"Mausam",2,4,2,4,5,6,65,20}
 s2={"Kumar",2,4,3,4,6,5,65,19,76}
 print(s1.union(s2))
-#print(s1.update(s2)).       
+#print(s1.update(s2)).       Sir why return o/p as None
 s1.update(s2)
 print(s1)
