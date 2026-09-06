e5ab8741d291c26bac6c387e38cc5e2218d3ecfa
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 18:08:44 +0000

    6 Sept 2026, 23:38

--- tutorial31.py
+++ tutorial31.py
@@ -4,7 +4,8 @@ print(type(empty_dic))
 empty_set=set()
 print(type(empty_set))
 
-s={2,4,2,5,7,9,7,"Mausam",True}
+l=["Mausam",2,4,3,9,2]
+
+s={2,4,2,5,7,9,7,"Mausam",True,l}
 print(s)
 #s[2]
-l=["Mausam",2,4,3,9,2]
