42d29b77f543539225b60ab97bdfae92d9cb95d3
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:02:08 +0000

    7 Sept 2026, 01:32

--- tutorial31b.py
+++ tutorial31b.py
@@ -1,4 +1,6 @@
 city1={"Tokyo","Madrid","Berlin","Delhi"}
 city2={"Tokyo","Seoul","Kabul","Madrid"}
 #print(city1.intersection(city2))
-print(city1.intersection_update(city2))
+#print(city1.intersection_update(city2)).        why return o/p as None
+city1.intersection_update(city2)
+print(city1)
