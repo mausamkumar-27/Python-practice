709bc5b278c56042a8d1d1b632e7df5a1abe7624
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:17:01 +0000

    7 Sept 2026, 01:47

--- tutorial31b.py
+++ tutorial31b.py
@@ -1,9 +1,9 @@
 city1={"Tokyo","Madrid","Berlin","Delhi"}
 city2={"Tokyo","Seoul","Kabul","Madrid"}
-print(city1.intersection(city2))
+'''print(city1.intersection(city2))
 #print(city1.intersection_update(city2)).        why return o/p as None
 city1.intersection_update(city2)
-print(city1)
+print(city1)'''
 
 
 print(city1.symmetric_difference(city2))
