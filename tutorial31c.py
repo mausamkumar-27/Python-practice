c35e196c76f5a00b862be1a560767419d3518a1a
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 20:37:31 +0000

    7 Sept 2026, 02:07

--- tutorial31c.py
+++ tutorial31c.py
@@ -1,3 +1,19 @@
 city1={"Tokyo","Madrid","Berlin","Delhi"}
 city2={"Tokyo","Seoul","Kabul","Madrid"}
 print(city1.isdisjoint(city2))
+print(city1.issuperset(city2))
+print(city1.issubset(city2))
+
+city1.add("Channai")
+print(city1)
+#city1.remove("Kolkata")          Keyerror dega bcz use removeand kolkata absent in city1
+#print(city1)
+
+city1.remove("Tokyo")
+print(city1)
+
+city1.discard("Kolkata")
+print(city1)                 #No KeyError bcz use discard
+
+city1.discard("Berlin")
+print(city1)
