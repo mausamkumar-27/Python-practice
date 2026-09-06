c15798e3f2a0ad52964db12e215bf9e0422e4a37
mausamkumar-27 <aapka_email@gmail.com>
2026-09-06 19:36:54 +0000

    7 Sept 2026, 01:06

--- tutorial31a.py
+++ tutorial31a.py
@@ -9,4 +9,6 @@ print(s1)
 
 city1={"Delhi","Mumbai","Londan","Hydrabad","Kota"}
 city2={"Patna","Indore","Wizag","Kota","Londan"}
-print(city1.intersection(s2))
+#print(city1.intersection(s2)).   Why return empty set
+city=city1.intersection(city2)
+print(city)
