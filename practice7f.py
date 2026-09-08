1ec916483046f1d7f0e10cf67f05bdc4d2c17de6
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 11:26:21 +0000

    8 Sept 2026, 16:56

--- practice7f.py
+++ practice7f.py
@@ -2,9 +2,11 @@ numbers=[12,7,19,24,33,46,51,68]
 evens=[]
 odds=[]
 for no in numbers:
-    if(no%2==0):
-        no.append(evens)
+    if no%2==0:
+        #no.append(evens)       beta list.append() hota hai
+        evens.append(no)
     else:
-        no.append(odds)
+        #no.append(odds)         beta list.append() hota hai
+        odds.append(no)
 print(evens)
 print(odds)
