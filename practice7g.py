e2e87829b98ab684769179824876e0b3ec55a24d
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 11:48:43 +0000

    8 Sept 2026, 17:18

--- practice7g.py
+++ practice7g.py
@@ -1,4 +1,6 @@
 correct_pin=1234
 attempts=3
 while attempts > 0:
-    int(input("Enter PIN: "))
+    entered_pin =int(input("Enter PIN: "))
+    if entered_pin==correct_pin:
+        print("Access Granted")
