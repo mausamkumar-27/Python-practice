35d10a0b00a69641d9186539267c531682fa80d5
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 02:33:52 +0000

    8 Sept 2026, 08:03

--- practice7a.py
+++ practice7a.py
@@ -1,4 +1,5 @@
 records=(("Mausam",85),("Harry",92),("Ali",78))
-for i in records:
-    print(i)
-print(f"{"Mausam","Harry","Ali"}:{85,92,78}")
+#for i in records:
+    #print(i)
+print(f"{"Mausam","Harry","Ali"}:{85,92,78}"for i in records)
+print(i)
