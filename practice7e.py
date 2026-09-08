30aa5718e31a313e1d55d18094caca840a3fba4e
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 11:11:55 +0000

    8 Sept 2026, 16:41

--- practice7e.py
+++ practice7e.py
@@ -1,1 +1,13 @@
 marks_list=[95,82,67,45,89,32,74]
+for mark in marks_list:
+    if(mark>=90):
+        print("Grade A+")
+    elif(mark>=80 and mark<90):
+        print("Grade A")
+    elif(mark>=60 and mark<80):
+        print("Grade B")
+    elif(mark>=40 and mark<60):
+        print("Grade C")
+    else:
+        print("Failed")
+    
