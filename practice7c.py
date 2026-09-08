e008c6e61146715b6e66d6b493f1b55deafccc04
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 09:47:33 +0000

    8 Sept 2026, 15:17

--- practice7c.py
+++ practice7c.py
@@ -2,6 +2,8 @@ inventory={
     "Apple":(120,15),        #price,stock
     "Banana":(40,0),
     "Mango":(80,25),
-    "Orangge":(60,0)
+    "Orange":(60,0)
 }
 out_of_stock=set()
+for item in inventory:
+    print(item)
