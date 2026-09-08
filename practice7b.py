f3a671349000ed28b03b0d4f6c4c58250e74a8e2
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 09:30:08 +0000

    8 Sept 2026, 15:00

--- practice7b.py
+++ practice7b.py
@@ -2,6 +2,3 @@ student_a={"Maths","Physics","Chemistry","English"}
 student_b={"Computer Science","Physics","Maths","Biology"}
 t=student_a.intersection(student_b)
 print(t)
-tup=tuple(t)
-print(tuple(t))
-print(type(t))
