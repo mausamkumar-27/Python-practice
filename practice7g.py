9169233f6337076fcb84a274134cbde40dd2a370
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 11:51:44 +0000

    8 Sept 2026, 17:21

--- practice7g.py
+++ practice7g.py
@@ -4,3 +4,8 @@ while attempts > 0:
     entered_pin =int(input("Enter PIN: "))
     if entered_pin==correct_pin:
         print("Access Granted")
+    else:
+        attempts-=1
+        print("Wrong PIN! Attempts Left: ",attempts)
+if attempts==0:
+    print("Account Locked")
