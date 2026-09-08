d6bb6b256ab6f3bc540e6c1cc591713658c784b8
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 12:57:02 +0000

    8 Sept 2026, 18:27

--- practice7i.py
+++ practice7i.py
@@ -2,5 +2,5 @@ raw_name="   mAuSaM kUmAr  "
 print(raw_name.strip(" "))
 print(raw_name.title())
 age=19
-name="Mausam Kumar"
+name=raw_name.strip().title()
 print(f"Hello {name},your age is {age}")
