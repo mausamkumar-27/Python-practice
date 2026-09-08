0692fca207d03c9203f8cb0ce58c7d40c7ffc969
mausamkumar-27 <aapka_email@gmail.com>
2026-09-08 02:55:18 +0000

    8 Sept 2026, 08:25

--- practice7a.py
+++ practice7a.py
@@ -1,10 +1,16 @@
 records=(("Mausam",85),("Harry",92),("Ali",78))
 student_dict={}
-for name,marks in records:
+'''for name,marks in records:
     student_dict[name]=marks            
 print(student_dict)
 print(student_dict.get("Ali"))
 #print(student_dict.get("Rohan"))
-print(student_dict.get("Rohan","Not Found"))
+print(student_dict.get("Rohan","Not Found"))'''
+
+
+
+
+
+student_dict={name:marks for name,marks in records}
 
 
