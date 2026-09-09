caec5a621b3dc471e34063924e9c635e2ddd383c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-09 10:58:12 +0000

    9 Sept 2026, 16:28

---
+++ practice8.py
@@ -0,0 +1,8 @@
+student_database = {
+    "st_101": ("Aman", ["Maths", "Physics", "Chemistry"], "85%"),
+    "st_102": ("Riya", ["Biology", "Physics"], "N/A"),
+    "st_103": ("Kunal", ["Maths", "Computer", "Physics"], "72%"),
+    "st_104": ("Pooja", ["Chemistry", "Biology", "English"], "91%"),
+    "st_105": ("Vikas", ["History", "English"], "invalid_entry"),
+    "st_106": ("Sneha", ["Maths", "Computer", "Chemistry"], "64%")
+}
