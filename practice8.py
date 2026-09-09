d364ecd65a9c58598b571caecb009588e67f4a43
mausamkumar-27 <aapka_email@gmail.com>
2026-09-09 11:34:37 +0000

    9 Sept 2026, 17:04

--- practice8.py
+++ practice8.py
@@ -7,6 +7,8 @@ student_database = {
     "st_106": ("Sneha", ["Maths", "Computer", "Chemistry"], "64%")
 }
 unique_course=set()
-for tup in student_database.items():
-    print(tup)
-    
+#for tup in student_database.items():
+   #print(tup)
+for st_id,(name,course,attendence) in student_database:
+   unique_course.update(course)
+   print(unique_course)
