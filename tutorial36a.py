b1d1d4baeb36adba41d7eed64cc3b0d620a48587
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 11:47:24 +0000

    10 Sept 2026, 17:17

--- tutorial36a.py
+++ tutorial36a.py
@@ -1,7 +1,8 @@
 num1=input("Enter 1st numbers: ")
 num2=input("Enter 2nd numbers: ")
-result=int(num1)/int(num2)
+
 try:
+    result=int(num1)/int(num2)
     print(f"Your ans is {result}")
 except ValueError:
     print("Please enter numbers only!")
