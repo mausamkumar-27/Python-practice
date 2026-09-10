faa9f5591783e67fbecde344ec79c7a423515e0d
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 10:38:53 +0000

    10 Sept 2026, 16:08

---
+++ tutorial35.py
@@ -0,0 +1,12 @@
+for i in range(6):
+    print(i)
+else:
+    print("Loop finished successfully!")
+
+
+for i in range(7):
+    print(i)
+    if i==3:
+        break
+else:
+    print("This will not execute!")
