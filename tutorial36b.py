e109240fcef530f1bef7a59f1e58d7eb5c9358fb
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 17:38:00 +0530

    use of index error

--- tutorial36b.py
+++ tutorial36b.py
@@ -2,7 +2,11 @@ languages=["Python","C","C++","Java"]
 idx=input("Enter index to search:")
 try:
     print(languages[int(idx)])
+#except (ValueError,IndexError,TypeError):
+    #print("Index out of bounds! valid range is 0 to 3")
+
+
 except IndexError:
-    print("Index out of bounds! valid range is 0 to 3")
+     print("Index out of bounds! valid range is 0 to3")
 except ValueError:
     print("Enter numbers only!")
