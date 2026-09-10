ecacf799417b607717443a4e00f357a9bafe064c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 11:38:04 +0000

    10 Sept 2026, 17:08

--- tutorial36a.py
+++ tutorial36a.py
@@ -1,9 +1,6 @@
 num1=input("Enter 1st numbers: ")
 num2=input("Enter 2nd numbers: ")
-
-new_num1=int(num1)
-new_num2=int(num2)
-division=num1/num2
+division=int(num1)/int(num2)
 try:
     print(f"Your ans is {division}")
 except ValueError:
