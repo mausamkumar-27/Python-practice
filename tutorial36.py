f8a6323c759a97bbefd46ee0c9f9b480a9b08413
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 11:20:15 +0000

    10 Sept 2026, 16:50

--- tutorial36.py
+++ tutorial36.py
@@ -1,14 +1,11 @@
 raw_inputs=["45","100","twenty","80","N/A","95"]
 valid_numbers=[]
 for items in raw_inputs:
-    print(items)
-try:
+    try:
     #new=int(items).valid_numbers.append()
-    new=int(items)
-    valid_numbers.append(new)
-    
-
-except ValueError:
-    print(f"skipping invalid:{items}")
+     new=int(items)
+     valid_numbers.append(new)
+    except ValueError:
+     print(f"skipping invalid:{items}")
 print(valid_numbers)
 
