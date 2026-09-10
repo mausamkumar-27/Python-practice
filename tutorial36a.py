18355aa2c74f21b43d2a22befeb3f8731631b4fb
mausamkumar-27 <aapka_email@gmail.com>
2026-09-10 11:43:19 +0000

    10 Sept 2026, 17:13

--- tutorial36a.py
+++ tutorial36a.py
@@ -1,9 +1,8 @@
 num1=input("Enter 1st numbers: ")
 num2=input("Enter 2nd numbers: ")
-division=num1/num2
 result=int(num1)/int(num2)
 try:
-    print(f"Your ans is {division}")
+    print(f"Your ans is {result}")
 except ValueError:
     print("Please enter numbers only!")
 except ZeroDivisionError:
