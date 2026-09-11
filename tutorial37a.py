85817dc0e0525c1ec94e5b2625ff114d0f74b410
mausamkumar-27 <aapka_email@gmail.com>
2026-09-11 02:45:06 +0000

    11 Sept 2026, 08:15

--- tutorial37a.py
+++ tutorial37a.py
@@ -1,8 +1,8 @@
 try:
     arr=[10,20]
     print(arr[9])
-#except ValueError:
-except IndexError:
+except ValueError:
+#except IndexError:
     print("Handled ValueError")
 finally:
     print("Closing active connection before system crash...")
