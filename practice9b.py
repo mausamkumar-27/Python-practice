868379536ec0a849080a2c46e06c3badc0ae15b1
mausamkumar-27 <aapka_email@gmail.com>
2026-09-11 08:30:24 +0000

    11 Sept 2026, 14:00

--- practice9b.py
+++ practice9b.py
@@ -1,5 +1,6 @@
-#age=int(input("Enter your age: "))          
+#age=int(input("Enter your age: "))          hmesha input try block ke andar lo,yahan if string("a","b") daal diya to program try block ke pehle hi crash ho jaayega
 try:
+    age=int(input("Enter your age: "))
     if age<0 or age>120:
         raise ValueError("age in between 0 to 120!")
 except:
