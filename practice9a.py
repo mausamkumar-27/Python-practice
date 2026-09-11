29c5cfa54bf8c48410b8daa9789b830ba37f3158
mausamkumar-27 <aapka_email@gmail.com>
2026-09-11 13:26:51 +0530

    accha sawal tha

--- practice9a.py
+++ practice9a.py
@@ -2,7 +2,7 @@ list=[5,7,9,11]
 name=input("Enter your name: ")
 try:
     print(list[2])             #if tu list me out of range index daalta to if name wala code run hi nhi krta bcz direct except pr chal jaata
-    if name=="Mausam":
+    if name=="Mausam":          #inside try block code execute from top to bottom
         print("Your name matches the criteria!")
 except ValueError:
     print("This is not your name!")
