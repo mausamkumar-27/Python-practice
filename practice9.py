65b86948e67345f707bbf3202d1a008ed214c293
mausamkumar-27 <aapka_email@gmail.com>
2026-09-11 06:41:25 +0000

    11 Sept 2026, 12:11

--- practice9.py
+++ practice9.py
@@ -1,2 +1,11 @@
 num1=input("Enter 1st no. ")
-num2=input("Enter ")
+num2=input("Enter 2nd no. ")
+try:
+    result=int(num1)/int(num2)
+    print(f"Your ans:{result}")
+except ValueError:
+    print("Please enter integers only!")
+except ZeroDivisionError:
+    print("Impossible to divide by Zero")
+else:
+    print("Result matches successfully!")
