e83a4e306b36711c33da2493d82cef73d542e562
mausamkumar-27 <aapka_email@gmail.com>
2026-09-11 02:23:55 +0000

    11 Sept 2026, 07:53

---
+++ tutorial37.py
@@ -0,0 +1,11 @@
+def check_execution():
+    try:
+        print("Executing try block...")
+
+    except Exception:
+        return 0
+    finally:
+        print("I will always execute,even after the return statment!")
+print("This standard print statement will never run")
+val=check_execution
+print("Function returned: ",val)
