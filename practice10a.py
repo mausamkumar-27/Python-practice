02d06fd941d41be6f2408b4e1a86a1ba7da2798a
mausamkumar-27 <aapka_email@gmail.com>
2026-09-12 20:11:26 +0000

    13 Sept 2026, 01:41

--- practice10a.py
+++ practice10a.py
@@ -2,6 +2,6 @@ def extract_unique_integer(data_tuple):
     s=set()
     for i in data_tuple:
         print(i)
-extract_unique_integer("Mausam",2,2.7,10,2,10,"Hello")
+extract_unique_integer(("Mausam",2,2.7,10,2,10,"Hello"))      #tumne kevl 1 parameter liye ho line 1 me isliye double (()) taaki ek tuple lge
 
     
