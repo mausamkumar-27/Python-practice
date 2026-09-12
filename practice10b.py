3278e71e295180a356b1b7696eec995e5a2fc4ce
mausamkumar-27 <aapka_email@gmail.com>
2026-09-12 21:11:42 +0000

    13 Sept 2026, 02:41

--- practice10b.py
+++ practice10b.py
@@ -1,6 +1,12 @@
 def validate_grades(grade_dict):
     d={}
-    for name,mark in grade_dict.item():
+    for name,mark in grade_dict.items():
         if mark<0 or mark>100:
             raise ValueError(f"Please! Enter valid marks")
+    try:
+        validate_grades({"Mausam":85,"Aman":90,"Priya":95,"Raman":120,"Chaman":-10})
+    except ValueError as e:
+        print("Caught error:",e)
+
+
         
