d490a54e20733312fb0dd982cbdf095ed3703157
mausamkumar-27 <aapka_email@gmail.com>
2026-09-12 19:38:23 +0000

    13 Sept 2026, 01:08

--- practice10a.py
+++ practice10a.py
@@ -1,6 +1,5 @@
 def extract_unique_integer(data_tuple):
     s=set()
     extract_unique_integer("Mausam",2,2.7,10,2,10,"Hello")
-for i in extract_unique_integer:
-    print(i)
-    
+    for i in data_tuple:
+        print(i)
