7e7850a3e2ee1a29e0df90911b48ef44181f8e5d
mausamkumar-27 <aapka_email@gmail.com>
2026-09-13 01:59:26 +0530

    accha sawal tha

--- practice10a.py
+++ practice10a.py
@@ -1,10 +1,11 @@
 def extract_unique_integer(data_tuple):
     s=set()
-    for i in data_tuple:
+    for i in data_tuple:              
         try:
             a=int(i)
             s.add(a)
         except ValueError:
             print("control the Error")
     print(s)
-extract_unique_integer(("Mausam",2,9.7,10,2,10,5,7,8,"Hello"))      #tumne kevl 1 parameter liye ho line 1 me isliye double (()) taaki ek tuple lge
+extract_unique_integer(("Mausam",2,9.7,10,2,10,5,7,8,"Hello"))     #tumne kevl 1 parameter liye ho line 1 me isliye double (()) taaki ek tuple lge
+#calling to beta last me hi hota hai
