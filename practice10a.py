1b557828d6e7c08c595a2dc82fd90f286629ddae
mausamkumar-27 <aapka_email@gmail.com>
2026-09-12 20:27:41 +0000

    13 Sept 2026, 01:57

--- practice10a.py
+++ practice10a.py
@@ -7,4 +7,4 @@ def extract_unique_integer(data_tuple):
         except ValueError:
             print("control the Error")
     print(s)
-extract_unique_integer(("Mausam",2,2.7,10,2,10,5,7,8,"Hello"))      #tumne kevl 1 parameter liye ho line 1 me isliye double (()) taaki ek tuple lge
+extract_unique_integer(("Mausam",2,9.7,10,2,10,5,7,8,"Hello"))      #tumne kevl 1 parameter liye ho line 1 me isliye double (()) taaki ek tuple lge
