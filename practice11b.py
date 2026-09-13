a47fb20f8d83cccd556f199b8a61f55b267a3e7c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-13 17:00:36 +0000

    13 Sept 2026, 22:30

--- practice11b.py
+++ practice11b.py
@@ -1,7 +1,18 @@
-def validate_salaries(salary_dict):
+'''def validate_salaries(salary_dict):
     for i in salary_dict.items():
         if i.values()<=0:
             raise ValueError(f"Salary can't be zero or negative for {i}")
         if i.values()>500000:
-            raise ValueError(f"Salary is unrealistically high for {i}")
+            raise ValueError(f"Salary is unrealistically high for {i}")'''
+
+
+
+
+def validate_salaries(salary_dict):
+    for name,salary in salary_dict.items():
+        if salary<=0:
+            raise ValueError(f"Salary can't be zero or negative for {name}")
+        if salary>500000:
+            raise ValueError(f"Salary is unrealistically high for {name}")
+        
         
