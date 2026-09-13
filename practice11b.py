c4b3f414f6f4bf9c7f851cea9677837d8def2047
mausamkumar-27 <aapka_email@gmail.com>
2026-09-13 17:20:53 +0000

    13 Sept 2026, 22:50

--- practice11b.py
+++ practice11b.py
@@ -16,7 +16,7 @@ def validate_salaries(salary_dict):
             raise ValueError(f"Salary is unrealistically high for {name}")
         
     try:
-        validate_salaries({"Hardik":-5000,"Raman":50000000})
+        validate_salaries({"Mausam":150000,"Aman":200000,"Hardik":-5000,"Raman":50000000})
     except ValueError:
         print(f"Your invalid salary is{name}:{salary}")
 validate_salaries({"Mausam":150000,"Aman":200000,"Hardik":-5000,"Raman":50000000})
