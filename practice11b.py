0ea040c0b778a9e6fbf5605a9f1c922968760ee5
mausamkumar-27 <aapka_email@gmail.com>
2026-09-13 17:22:31 +0000

    13 Sept 2026, 22:52

--- practice11b.py
+++ practice11b.py
@@ -19,4 +19,4 @@ def validate_salaries(salary_dict):
         validate_salaries({"Mausam":150000,"Aman":200000,"Hardik":-5000,"Raman":50000000})
     except ValueError:
         print(f"Your invalid salary is{name}:{salary}")
-validate_salaries({"Mausam":150000,"Aman":200000,"Hardik":-5000,"Raman":50000000})
+#validate_salaries({"Mausam":150000,"Aman":200000,"Hardik":-5000,"Raman":50000000})
