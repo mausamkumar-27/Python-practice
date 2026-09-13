8285dff591e58f58b9e94d506dee923424692436
mausamkumar-27 <aapka_email@gmail.com>
2026-09-13 14:59:38 +0000

    13 Sept 2026, 20:29

--- practice11.py
+++ practice11.py
@@ -2,8 +2,12 @@ def process_mixed_data(data_list):
     s=set()
     for i in data_list:
         #s.update(i)
-        s.add(i)           #add uses for single element only--haan for loop me i single element hi hai
+        #s.add(i)           #add uses for single element only--haan for loop me i single element hi hai
+    
+        try:
+            val=int(i)
+            s.add(val)
+        except ValueError:
+            print(f"skkiping invalid integer!")
     print(s)
-
-
 process_mixed_data(("Mausam",9,122,122,9,7,56,7,"Hardik","Aman"))
