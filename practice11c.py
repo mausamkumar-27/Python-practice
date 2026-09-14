49826737c91855a4da56497bea78ab7e17713f31
mausamkumar-27 <aapka_email@gmail.com>
2026-09-14 09:02:51 +0000

    14 Sept 2026, 14:32

--- practice11c.py
+++ practice11c.py
@@ -23,3 +23,11 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print("*",end=" ")
     print()
+
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i+j,end=" ")
+    print()
+
