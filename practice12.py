f3d3e2185ebe175a7366f08b93f145ec3d81801f
mausamkumar-27 <aapka_email@gmail.com>
2026-09-14 12:23:01 +0000

    14 Sept 2026, 17:53

--- practice12.py
+++ practice12.py
@@ -1,4 +1,4 @@
-n=int(input())
+'''n=int(input())
 for i in range(1,n+1):
     for j in range(1,i+1):
         print("*",end=" ")        #Horizontally cursor ko rok rha hu according to inner loop condition
@@ -14,5 +14,12 @@ n=int(input())
 for i in range(1,n+1):
     for j in range(1,i+1):
         print(j,end=" ")
+    print()'''
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i*j,end=" ")
     print()
+
 
