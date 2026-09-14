a1098e39c37bd21cbe7add91cc64e9802d96c5df
mausamkumar-27 <aapka_email@gmail.com>
2026-09-14 11:55:36 +0000

    14 Sept 2026, 17:25

--- classques.py
+++ classques.py
@@ -17,14 +17,6 @@ for i in range(1,n+1):
         print(j,end=" ")
     print()
 
-
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print("*",end=" ")
-    print()
-
-
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
