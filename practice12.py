f9d4efa6c79e8586de06119b0b8d961e2477c97a
mausamkumar-27 <aapka_email@gmail.com>
2026-09-14 12:28:47 +0000

    14 Sept 2026, 17:58

--- practice12.py
+++ practice12.py
@@ -22,4 +22,12 @@ for i in range(1,n+1):
         print(i*j,end=" ")
     print()
 
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i*j,end=" ")
+    print()
+
+
+
 
