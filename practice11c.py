ff9b4d35c14abe01d9f25c8430f02153f29bfe85
mausamkumar-27 <aapka_email@gmail.com>
2026-09-14 09:25:10 +0000

    14 Sept 2026, 14:55

--- practice11c.py
+++ practice11c.py
@@ -29,7 +29,7 @@ n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
         print(i*j,end=" ")
-    print()'''
+    print()
 
 
 n=int(input())
@@ -38,6 +38,13 @@ for i in range(1,n+1):
     for j in range(1,s+1):
         print(" ",end=" ")
     for k in range(1,i+1):
+        print("*",end=" ")
+    print()'''
+
+#print shape of H by * 
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
         print("*",end=" ")
     print()
 
