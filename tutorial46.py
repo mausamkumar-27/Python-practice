483e397207afbe44fa34b326503127b9ab060fdc
mausamkumar-27 <aapka_email@gmail.com>
2026-09-15 12:21:19 +0000

    15 Sept 2026, 17:51

--- tutorial46.py
+++ tutorial46.py
@@ -9,7 +9,16 @@ print(items)
 import os
 if not os.path.exists("test_dir"):
     os.mkdir("test_dir")
-    print("Directory created is successfully!")'''
+    print("Directory created is successfully!")
 
 import os
 os.rename("test_dir","retest_dir")
+
+import os
+os.rmdir("retest_dir")'''            #lo ye file delete bhi kr diya
+
+
+import os 
+if os.path.exists("virtual_envy.py"):
+    print("The file exists!")
+    
