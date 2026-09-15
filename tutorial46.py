2288b23c9a91ef93fcbe7e83bc78e0606f4184a3
mausamkumar-27 <aapka_email@gmail.com>
2026-09-15 11:57:34 +0000

    15 Sept 2026, 17:27

--- tutorial46.py
+++ tutorial46.py
@@ -1,3 +1,7 @@
-import os
+'''import os
 current_dir=os.getcwd()
-print("Current Directory: ",current_dir)
+print("Current Directory: ",current_dir)'''
+
+import os
+items=os.listdir()
+print(items)
