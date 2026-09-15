b73c8431e644af08d493a86edbba03081c0da1dd
mausamkumar-27 <aapka_email@gmail.com>
2026-09-15 12:05:30 +0000

    15 Sept 2026, 17:35

--- tutorial46.py
+++ tutorial46.py
@@ -1,7 +1,12 @@
 '''import os
 current_dir=os.getcwd()
-print("Current Directory: ",current_dir)'''
+print("Current Directory: ",current_dir)
 
 import os
 items=os.listdir()
-print(items)
+print(items)'''
+
+import os
+if not os.path.exists("test_dir"):
+    os.mkdir("test_dir")
+    print("Directory created is successfully!")
