2a8d9abb4ea87bb59aaf4eba4727329db4a347ed
mausamkumar-27 <aapka_email@gmail.com>
2026-09-16 12:45:19 +0000

    16 Sept 2026, 18:15

--- practice14a.py
+++ practice14a.py
@@ -5,3 +5,7 @@ for i in range(1,6):
     score=int(input("Enter score: "))
     list_of_tuple.append((string_name,score))
 print(list_of_tuple)
+for string_name,score in list_of_tuple:
+    if len(string_name)>=3:
+        d[string_name]=score
+print(d)
