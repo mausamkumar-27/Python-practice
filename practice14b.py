4e237eee6b262105b484078a8a8debc58daff12c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-16 13:06:02 +0000

    16 Sept 2026, 18:36

---
+++ practice14b.py
@@ -0,0 +1,13 @@
+s1=set()
+s2=set()
+for i in range(1,6):
+    n1=int(input("Enter n1: "))
+    n2=int(input("Enter n2: "))
+    s1.add(n1)
+    s2.add(n2)
+s=s1.intersection(s2)
+t=tuple(s)
+for j in t:
+    if j%2!=0:
+        sum+=j
+    print(sum)
