a35f68fc03b3708a3ba1ca9246a89be573ff291e
mausamkumar-27 <aapka_email@gmail.com>
2026-09-16 12:49:05 +0000

    16 Sept 2026, 18:19

--- practice14a.py
+++ practice14a.py
@@ -6,6 +6,6 @@ for i in range(1,6):
     list_of_tuple.append((string_name,score))
 print(list_of_tuple)
 for string_name,score in list_of_tuple:
-    if len(string_name)>=3:
+    if len(string_name)>3:
         d[string_name]=score
 print(d)
