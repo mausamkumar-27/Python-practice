44cea1d4f8533a28429ee1e16f6f6573a795a4b5
mausamkumar-27 <aapka_email@gmail.com>
2026-09-17 12:54:25 +0000

    17 Sept 2026, 18:24

--- practice18a.py
+++ practice18a.py
@@ -1,6 +1,9 @@
+l=[]
 for i in range(1,6):
-    name=input("Enter names: ")
-    #l=list(i)  # inside list only iterable element valid & here i is integer
-    tup=tuple(i)
-    l=list(tup)
+    names=input("Enter names: ")
+    #l=list(i)  # in keyword ke andar keval iterable items valid h
+    #tup=tuple(i)   same as above reason
+    #l=list(tup)      same reason
+    l.append(names)
+
 print(l)
