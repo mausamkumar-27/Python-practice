682cb44e7520da0cd6d86d6f6a925907f199707f
mausamkumar-27 <aapka_email@gmail.com>
2026-09-17 12:35:37 +0000

    17 Sept 2026, 18:05

--- practice18a.py
+++ practice18a.py
@@ -1,3 +1,6 @@
-'''for i in range(1,6):
+for i in range(1,6):
     name=input("Enter names: ")
-    l=list(i)'''  # inside list only iterable element valid & here i is integer
+    #l=list(i)  # inside list only iterable element valid & here i is integer
+    tup=tuple(i)
+    l=list(tup)
+print(l)
