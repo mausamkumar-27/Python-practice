617650766e28be2f34b8b63034bb4ab6f2a85099
mausamkumar-27 <aapka_email@gmail.com>
2026-09-17 13:24:38 +0000

    17 Sept 2026, 18:54

--- practice18a.py
+++ practice18a.py
@@ -7,4 +7,10 @@ for i in range(1,6):
     #l=list(tup)      same reason
     l.append(names)
     tup=tuple(l)
-print(tup)
+vowel_list=[]
+consonent_list=[]
+for j in tup:
+    if j[0].lower() in ('a','e','i','o','u'):
+        vowel_list.append(j)
+    else:
+        consonent_list.append(j)
