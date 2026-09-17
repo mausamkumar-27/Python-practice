6c69ff8a3bec7f012013aa7c661046dd4b4bcc10
mausamkumar-27 <aapka_email@gmail.com>
2026-09-17 12:56:18 +0000

    17 Sept 2026, 18:26

--- practice18a.py
+++ practice18a.py
@@ -1,9 +1,10 @@
 l=[]
+
 for i in range(1,6):
     names=input("Enter names: ")
     #l=list(i)  # in keyword ke andar keval iterable items valid h
     #tup=tuple(i)   same as above reason
     #l=list(tup)      same reason
     l.append(names)
-
-print(l)
+    tup=tuple(l)
+    
