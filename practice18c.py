a7a05352d1dc3e1c762ff55a030cc4a42032a678
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 07:35:31 +0000

    18 Sept 2026, 13:05

--- practice18c.py
+++ practice18c.py
@@ -2,4 +2,7 @@
 sentence=input("Enter sentence: ")
 #sentence.split()   string ek immutable hota ha so variable assign kr
 words=sentence.split()
-print(words)
+#print(words)
+for i in words:
+    reverse_word=i.reverse()
+    print(reverse_word)
