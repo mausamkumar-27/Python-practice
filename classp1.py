e6ff93f977e2e5f55461a857284c3f0c48f405d4
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 17:02:04 +0530

    accha revision karaya

--- classp1.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j==1 or j==n or i==(n/2+1):
-            print("*",end=" ")
-    print()
--- practice18c.py
+++ practice18c.py
@@ -8,5 +8,8 @@ for i in words:
     reverse_word=i[::-1]
     #print(reverse_word)
     #d[len(reverse_word)]=tuple(reverse_word)      use comma bcz ye reverse_word ka char as tuple dega 
-    d[len(reverse_word)]=tuple(reverse_word,)  
+    #d[len(reverse_word)]=tuple(reverse_word,)     for add multipl item use .get() method othrwse overwrite problem create
+    #d[len(reverse_word)]=tuple(d.get(reverse_word))    inside .get() you forgot to give key
+    #d[len(reverse_word)]=d.get(len(reverse_word), ())+ reverse_word   only concatenate tuple to tuple  not string to tuple
+    d[len(reverse_word)]=d.get(len(reverse_word), ())+ (reverse_word ,)  # trailling comma bhi lga
 print(d)
