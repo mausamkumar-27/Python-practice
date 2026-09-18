f29fb898b8a7a260636cd2e8e998c97884f156fc
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 07:47:47 +0000

    18 Sept 2026, 13:17

--- practice18c.py
+++ practice18c.py
@@ -1,9 +1,9 @@
-
+d={}
 sentence=input("Enter sentence: ")
 #sentence.split()   string ek immutable hota ha so variable assign kr
 words=sentence.split()
-print(words)
+#print(words)
 for i in words:
     #reverse_word=i.reverse()    use reverse() method only for list & not for string
     reverse_word=i[::-1]
-    print(reverse_word)
+    #print(reverse_word)
