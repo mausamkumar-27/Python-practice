24aa3e6bc07885ab387e6392bbb84dd8d2ae1f2c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 18:32:05 +0000

    19 Sept 2026, 00:02

--- star_pattern1.py
+++ star_pattern1.py
@@ -1,8 +1,9 @@
 n=int(input())
 for i in range(1,n+1):
-   print(" "*(n-i),end=" ")
-   for j in range(1,i+1):
-      print(i,end=" ")
-print()
+   for j in range(1,n-i+1):
+      print(" ",end=" ")
+      for k in range(1,i+1):
+         print(i,end=" ")
+      print()
 
       
