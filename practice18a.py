f6b628dee549a2d096fa14e8ed2ccd86f2521002
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 06:27:16 +0000

    18 Sept 2026, 11:57

--- practice18a.py
+++ practice18a.py
@@ -19,8 +19,8 @@ for j in tup:
 #print(vowel_list)
 #print(consonent_list)
 for vowel in vowel_list:
-    d[vowel]=d.get(vowel,0)+vowel_list
+    d[vowel]=d.get(vowel)+vowel_list
 for consnent in consonent_list:
-    d[consonent]=d.get(consonent,0)+consonent_list
+    d[consonent]=d.get(consonent)+consonent_list
 print(d)
 
