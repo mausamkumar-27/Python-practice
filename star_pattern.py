59e72ca6e09bd3f5204d20f23ad0af4aab687fd8
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 15:02:33 +0000

    18 Sept 2026, 20:32

--- star_pattern.py
+++ star_pattern.py
@@ -7,7 +7,7 @@ for i in range(1,n+1):
 n=int(input())
 for i in range(1,n+1):
     star=2*i-1
-    space=(2*n-star)//2
+    space=(2*n-star)
     print(space*" ", end=" ")
     #print(star*"* ")
     print("* "*star)
