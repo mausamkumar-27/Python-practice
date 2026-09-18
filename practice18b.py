b064cfc5817bf54a42c6cba69ce21a6b2eaf4495
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 07:05:55 +0000

    18 Sept 2026, 12:35

--- practice18b.py
+++ practice18b.py
@@ -4,9 +4,9 @@ def user(*names):
     consonant_list=[]
     for i in names:
         if i[0].lower() in ('a','e','i','o','u'):
-            i.append(vowel_list)
+            vowel_list.append(i)
         else:
-            i.append(consonant_list)
+            consonant_list.append(i)
     d['vowel']=tuple(vowel_list)
     d['consonant']=tuple(consonant_list)
     print(d)
