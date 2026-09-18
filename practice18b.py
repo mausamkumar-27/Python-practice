8e9d3d9c9cefd4dc7d7185704260d04106f70d8f
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 07:03:26 +0000

    18 Sept 2026, 12:33

---
+++ practice18b.py
@@ -0,0 +1,13 @@
+def user(*names):
+    d={}
+    vowel_list=[]
+    consonant_list=[]
+    for i in names:
+        if i[0].lower() in ('a','e','i','o','u'):
+            i.append(vowel_list)
+        else:
+            i.append(consonant_list)
+    d['vowel']=tuple(vowel_list)
+    d['consonant']=tuple(consonant_list)
+    print(d)
+user('Mausam','hardik','ram','Utthappa','Elvish','Aman')
