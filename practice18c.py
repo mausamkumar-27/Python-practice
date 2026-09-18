07953354b9640b2960fc8b6cf307fb3c1a346c73
mausamkumar-27 <aapka_email@gmail.com>
2026-09-18 07:48:56 +0000

    18 Sept 2026, 13:18

--- practice18c.py
+++ practice18c.py
@@ -7,3 +7,5 @@ for i in words:
     #reverse_word=i.reverse()    use reverse() method only for list & not for string
     reverse_word=i[::-1]
     #print(reverse_word)
+    d[len(reverse_word)]=tuple(reverse_word)
+print(d)
