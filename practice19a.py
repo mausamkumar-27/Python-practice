8abb17edbe86f795dcc18e3d9122ad3082cfbe03
mausamkumar-27 <aapka_email@gmail.com>
2026-09-19 12:55:07 +0000

    19 Sept 2026, 18:25

--- practice19a.py
+++ practice19a.py
@@ -8,11 +8,11 @@ for i in range(1,4):
 #print(d)
 #new_set=set((d.(values)).intersection(skill))      set ke andar immutable item hi jaayegi
 
-all_skills=set().union(*d.values())
-print("all_skills: " ,all_skills)   # ye * teeno set ko alag-2 open kiya & union leke set me daal diya
-'''common_skills=list(d.values())[0].intersection(*d.values())
-
-summary={
+#all_skills=set().union(*d.values())
+#print("all_skills: " ,all_skills)   # ye * teeno set ko alag-2 open kiya & union leke set me daal diya
+common_skills=list(d.values())[0].intersection(*d.values())
+print("common_skills: ",common_skills)
+'''summary={
     "common_skills":common_skills,
     "all_team_skills":all_skills
 }
