85e37cb0a104855e47a5336722a5e98c282d282e
mausamkumar-27 <aapka_email@gmail.com>
2026-09-19 10:07:45 +0000

    19 Sept 2026, 15:37

---
+++ practice19.py
@@ -0,0 +1,17 @@
+d={}
+s=set()
+sentence=input("Enter sentence: ")
+replace_space=sentence.replace(" ","")
+for i in replace_space:
+    s.update(replace_space)
+#print(s)
+vowel_set=set()
+consonent_set=set()
+for j in s:
+    if j==('a','e','i','o','u','A','E','I','O','U'):
+        vowel_set.update(j)
+    else:
+        consonent_set.update(j)
+d[vowel_set]=d.get(vowel_set,0)+1
+d[consonent_set]=d.get(consonent_set,0)+1
+print(d)
