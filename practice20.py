d16a70f057ee010ab6a42ca02abe712bd927e569
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 12:05:04 +0000

    21 Sept 2026, 17:35

--- practice20.py
+++ practice20.py
@@ -1,6 +1,6 @@
 
 
-
+d={}
 tup=()
 for i in range(1,4):
     names=input("Name: ")
@@ -9,4 +9,5 @@ for i in range(1,4):
     marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
 #tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments
 #tup=(roll,section)    overwrite problem created bcz using of assignment operator
+    d['n']=d.get('n',())+tup
 print(tup)
