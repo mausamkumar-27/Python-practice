9365fec523436d336d48c6ba9d1082331786b027
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 13:00:37 +0000

    21 Sept 2026, 18:30

--- practice20.py
+++ practice20.py
@@ -6,7 +6,7 @@ def manage_students():
         name=input("Name: ")
         roll=int(input("Roll No.: "))
         section=input("section: ")
-        tup=(roll,section)
+        tup=(roll,section)          # bahi tuple python me aise hi to bnta h bhul gye
         marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
 #tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments & kisi list,string etc ko tuple me badalta h
 #tup=(roll,section)    overwrite problem created bcz using of assignment operator
