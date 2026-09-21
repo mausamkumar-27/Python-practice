0c305532e29aa9e7413d059209b37836cf76df9c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 12:36:23 +0000

    21 Sept 2026, 18:06

--- practice20.py
+++ practice20.py
@@ -11,12 +11,12 @@ for i in range(1,4):
     marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
 #tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments
 #tup=(roll,section)    overwrite problem created bcz using of assignment operator
-    sum=0
+    
     d[roll]={
        "Students Name":name,
        "Roll & Sections":tup,
        "Marks list":marks,
-       "Total Marks":sum()
+       "Total Marks":sum(marks)
    }
 for Roll_no,Record in d.items():
     print("f Roll_no {Roll_no}:{recprd}")
