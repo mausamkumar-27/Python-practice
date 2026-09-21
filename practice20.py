f92969f986224cc23afad830e156163311cfd64c
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 11:40:22 +0000

    21 Sept 2026, 17:10

--- practice20.py
+++ practice20.py
@@ -6,3 +6,6 @@ for i in range(1,4):
     names=input("Name: ")
     roll=int(input("Roll No.: "))
     section=input("section: ")
+    marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
+tup=tuple(roll,section)
+print(tup)
