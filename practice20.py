e95112b9129c1c8d829b6821a128d33d43ea2b72
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 11:51:59 +0000

    21 Sept 2026, 17:21

--- practice20.py
+++ practice20.py
@@ -1,12 +1,12 @@
 
 
 
-tup=tuple()
+tup=()
 for i in range(1,4):
     names=input("Name: ")
     roll=int(input("Roll No.: "))
     section=input("section: ")
     marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
 #tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments
-
+tup=(roll,section)
 print(tup)
