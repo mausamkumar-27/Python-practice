4276eca57a974698d750e04b2e2fb8d63653a3de
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 12:58:55 +0000

    21 Sept 2026, 18:28

--- practice20.py
+++ practice20.py
@@ -8,7 +8,7 @@ def manage_students():
         section=input("section: ")
         tup=(roll,section)
         marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
-#tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments
+#tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments & kisi list,string etc ko tuple me badalta h
 #tup=(roll,section)    overwrite problem created bcz using of assignment operator
     
         d[roll]={
