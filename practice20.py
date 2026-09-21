31ea3f9534a219e558b3b07d1fa89b007c2cacd2
mausamkumar-27 <aapka_email@gmail.com>
2026-09-21 12:39:00 +0000

    21 Sept 2026, 18:09

--- practice20.py
+++ practice20.py
@@ -2,21 +2,21 @@
 def manage_students():
     d={}
 
-for i in range(1,4):
-    name=input("Name: ")
-    roll=int(input("Roll No.: "))
-    section=input("section: ")
-    tup=(roll,section)
-    marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
+    for i in range(1,4):
+        name=input("Name: ")
+        roll=int(input("Roll No.: "))
+        section=input("section: ")
+        tup=(roll,section)
+        marks=[int(input("sub 1 marks: ")),int(input("sub 2 marks: ")),int(input("sub 3 marks: "))]
 #tup=tuple(roll,section)    tumne yahan 2 alag-2 variables daal diye jbki tuple accept only 1 arguments
 #tup=(roll,section)    overwrite problem created bcz using of assignment operator
     
-    d[roll]={
+        d[roll]={
        "Students Name":name,
        "Roll & Sections":tup,
        "Marks list":marks,
        "Total Marks":sum(marks)
    }
-for Roll_no,Record in d.items():
-    print("f Roll_no {Roll_no}:{recprd}")
+    for Roll_no,Record in d.items():
+        print("f Roll_no {Roll_no}:{recprd}")
 manage_students()
